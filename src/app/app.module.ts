1547569a0add2450a831303b52d66e1945b3f052
Manh. Nguyen Duc - CMC Global DU3.1 <ndmanh2@cmcglobal.vn>
2022-04-05 11:37:36 +0700

    feat(scully): add scully and sample blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScullyLibModule,
+    RouterModule.forRoot([
+      {
+        path: 'posts',
+        loadChildren: () =>
+          import('./blog/blog.module').then((m) => m.BlogModule),
+      },
+      { path: '**', component: AppComponent },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
